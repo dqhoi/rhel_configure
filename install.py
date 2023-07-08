e417bab1d168391ce9032bd04546346d1482f3de
hoidq0 <daoquochoi06092002@gmail.com>
2023-07-08 11:01:02 +0700

    add file

--- install.py
+++ install.py
@@ -141,10 +141,12 @@ def main():
     if os.path.exists(folder_path):
         for filename in os.listdir(folder_path):
             file_path = os.path.join(folder_path, filename)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
+            if filename != "log.txt":
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+
     delete_completed_functions()
 
 
