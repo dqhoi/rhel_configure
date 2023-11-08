28130ac2dcf35a3129da697905a3bd89585247d7
dqhoi <hoidqwork@gmail.com>
2023-11-08 16:52:22 +0700

    edit wifi install

--- wifi_install/wifi_install.sh
+++ wifi_install/wifi_install.sh
@@ -2,7 +2,7 @@
 source variables.sh
 
 wifi_install() {
-	if nmcli device status | grep "unavailable" | grep -q "wifi"; then
+	if nmcli device status | grep "unavailable" | grep -q "ethernet"; then
 		cd $REPO_DIR
 		wifi_first=$(ls iw* 2>/dev/null | head -n 1)
 		wifi_second=$(ls wireless-regdb* 2>/dev/null | head -n 1)
@@ -10,18 +10,29 @@ wifi_install() {
 		wifi_fourth=$(ls NetworkManager-wifi* 2>/dev/null | head -n 1)
 		if [ -n "$wifi_first" ]; then
 			sudo rpm -ivh $wifi_first
+		else
+			echo "No wifi rpm found"
+			break
 		fi
 		if [ -n "$wifi_second" ]; then
 			sudo rpm -ivh $wifi_second
+		else
+			echo "No wifi rpm found"
+			break
 		fi
 		if [ -n "$wifi_third" ]; then
 			sudo rpm -ivh $wifi_third
+		else
+			echo "No wifi rpm found"
+			break
 		fi
 		if [ -n "$wifi_fourth" ]; then
 			sudo rpm -ivh $wifi_fourth
+		else
+			echo "No wifi rpm found"
+			break
 		fi
 		sudo systemctl restart NetworkManager
-		sleep 30
 		sudo dnf reinstall iw wireless-regdb wpa_supplicant NetworkManager-wifi -y
 	fi
 	sudo dnf upgrade -y
